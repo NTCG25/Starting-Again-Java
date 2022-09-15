a6bfaac9fd74b265f4c8fe61ed940742391bcb93
NTCG25 <65662881+NTCG25@users.noreply.github.com>
2022-09-15 03:59:00 -0500

    Add files via upload

---
+++ Pounds to Kilo Conversion.java
@@ -0,0 +1,12 @@
+package com.nigelmar;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    double numPounds = 200d;
+	    double convertedKilograms = numPounds * 0.45359237d;
+        System.out.println("Kilograms = " + convertedKilograms);
+        // 90.7185 Kilos
+
+    }
+}
